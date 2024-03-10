1b87ca86e99975828666083b9aed1b3928500152
Nate Wang <supnate@gmail.com>
2024-03-10 10:31:26 +0800

    Update base for github pages.

--- packages/examples-antd/vite.config.ts
+++ packages/examples-antd/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'antd',
+  base: '/nice-form-react/antd',
   plugins: [react()],
   build: {
     outDir: '../../gh-pages/antd',
