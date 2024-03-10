c33ab9a59e13651a143eeffad6e11f170dbe1417
Nate Wang <supnate@gmail.com>
2024-03-10 10:23:46 +0800

    Create action to publish examples.

--- packages/examples-antd/vite.config.ts
+++ packages/examples-antd/vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'antd',
   plugins: [react()],
+  build: {
+    outDir: '../../gh-pages/antd',
+  },
 });
