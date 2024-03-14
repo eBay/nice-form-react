11bb74cc14147f331d3edec3ab85d7969245859b
Nate Wang <supnate@gmail.com>
2024-03-14 09:00:10 +0800

    Update base for formik examples.

--- packages/examples-formik/vite.config.ts
+++ packages/examples-formik/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nice-form-react/formik',
   plugins: [react()],
   build: {
     outDir: '../../gh-pages/formik',
